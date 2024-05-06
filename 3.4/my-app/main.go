8a51a8ed47b2974b711f1267049024f0dbd53108
jin1125 <shogo.jinta@gmail.com>
2024-05-06 12:59:29 +0900

    3.4

--- 3.4/my-app/main.go
+++ 3.4/my-app/main.go
@@ -7,10 +7,13 @@ func main() {
 	for i := 0; i < 100; i++ {
 		a = append(a, i)
 	}
-
 	n := 50
 	a = a[:n+copy(a[n:], a[n+1:])] // a = a[:50+49]
 	fmt.Println(a)
 
-	
+	s1 := "Hello"
+	b := []byte(s1)
+	b[0] = 'h'
+	s1 = string(b)
+	fmt.Println(s1)
 }
